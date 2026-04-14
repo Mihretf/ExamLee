ad688d3b659c17a90615b9bc58efeb782b998140
Mihretf <mihretworku94@gmail.com>
2026-04-14 20:19:22 +0300

    feat: implement protect middleware and user profile route

--- backend/src/utils/server.js
+++ backend/src/utils/server.js
@@ -13,6 +13,7 @@ app.use(express.json());
 
 // --- ADD THIS LINE HERE ---
 app.use('/api/auth', require('../routes/auth.routes')); 
+app.use('/api/users' , require('../routes/user.routes'));
 // --------------------------
 
 app.get("/", (req, res) => {
